6d120e820fe4fc6e0fa79517e0972fc97408d1a0
POFK <maotianxiang@bao.ac.cn>
2019-09-11 17:41:34 +0800

    init for procudtion

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ def make_shell_context():
     return {'db': db, 'User': User}
 
 if __name__ == "__main__":
-#   app.run(host='0.0.0.0', port=10299)
-    app.run()
+    app.run(host='0.0.0.0', port=10299)
+#   app.run()
